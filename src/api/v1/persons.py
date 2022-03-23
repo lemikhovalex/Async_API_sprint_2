cdc430331ab3d1167ad93710c77eddee846ef82d
Gee Hjwić <ghj096@gmail.com>
2022-03-23 03:05:55 +0300

    Add /persons endpoint

--- src/api/v1/persons.py
+++ src/api/v1/persons.py
@@ -1,7 +1,25 @@
-from pydantic import BaseModel
+from http import HTTPStatus
 from uuid import UUID
+
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
+from services.persons import PersonService, get_person_service
+
+router = APIRouter()
 
 
 class PersonPartial(BaseModel):
     uuid: UUID
     full_name: str
+
+
+@router.get('/{person_id}', response_model=PersonPartial)
+async def person_details(
+    person_id: str, person_service: PersonService = Depends(get_person_service)
+) -> PersonPartial:
+    person = await person_service.get_by_id(person_id)
+    if not person:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='person not found'
+        )
+    return PersonPartial(**person.dict())
---
+++ src/services/persons.py
@@ -0,0 +1,26 @@
+from functools import lru_cache
+
+from aioredis import Redis
+from db.elastic import get_elastic
+from db.redis import get_redis
+from elasticsearch import AsyncElasticsearch
+from fastapi import Depends
+from models.person import Person
+from .base import BaseService
+
+
+class PersonService(BaseService):
+
+    def _index_name(self):
+        return 'persons'
+
+    def _result_class(self):
+        return Person
+
+
+@lru_cache()
+def get_person_service(
+    redis: Redis = Depends(get_redis),
+    elastic: AsyncElasticsearch = Depends(get_elastic),
+) -> PersonService:
+    return PersonService(redis, elastic)
