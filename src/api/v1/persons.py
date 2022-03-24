4d32aa087eef09c8130183d18774a37d1b83367d
Gee Hjwić <ghj096@gmail.com>
2022-03-25 00:24:20 +0300

    Add /persons/ endpoint

--- src/api/v1/persons.py
+++ src/api/v1/persons.py
@@ -1,7 +1,8 @@
 from http import HTTPStatus
+from typing import List
 from uuid import UUID
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from pydantic import BaseModel
 from services.persons import PersonService, get_person_service
 
@@ -23,3 +24,15 @@ async def person_details(
             status_code=HTTPStatus.NOT_FOUND, detail='person not found'
         )
     return PersonPartial(**person.dict())
+
+@router.get('/', response_model=List[PersonPartial])
+async def persons(
+    page_size: int = Query(50, alias="page[size]"),
+    page_number: int = Query(1, alias="page[number]"),
+    person_service: PersonService = Depends(get_person_service)
+) -> List[PersonPartial]:
+    persons = await person_service.get_by(
+        page_number=page_number,
+        page_size=page_size,
+    )
+    return [ PersonPartial(**person.dict()) for person in persons ]
