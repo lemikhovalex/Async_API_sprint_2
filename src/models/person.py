211d512828a206636fcc2bd3166ee8f4ad528be7
Gee Hjwić <ghj096@gmail.com>
2022-03-26 19:45:39 +0300

    Add alias for person name field to comply with elasticsearch index

--- src/models/person.py
+++ src/models/person.py
@@ -7,7 +7,7 @@ class IDNamePerson(BaseModel):
     full_name: str
 
     class Config:
-        fields = {'uuid': 'id'}
+        fields = {'uuid': 'id', 'full_name': 'name'}
 
 
 class Person(IDNamePerson):
