5588ad83e6491abd2930ea94a753bed63a3e9b24
Aleksand Lemikhov <lemikhovalex@gmail.com>
2022-03-27 17:23:24 +0300

    cache injection

--- src/api/v1/genres.py
+++ src/api/v1/genres.py
@@ -2,14 +2,17 @@ from http import HTTPStatus
 from typing import List
 from uuid import UUID
 
+from fastapi_cache.decorator import cache
 from fastapi import APIRouter, Depends, HTTPException, Query
 from services.films import FilmService, get_film_service
 from services.genres import GenreService, get_genre_service
 from api.v1 import PartialFilmInfo, GenrePartial
+from core.config import REDIS_CACHE_EXPIRE
 
 router = APIRouter()
 
 @router.get('/{genre_id}', response_model=GenrePartial)
+@cache(expire=REDIS_CACHE_EXPIRE)
 async def genre_details(
     genre_id: str, genre_service: GenreService = Depends(get_genre_service)
 ) -> GenrePartial:
@@ -21,6 +24,7 @@ async def genre_details(
     return GenrePartial(**genre.dict())
 
 @router.get('/', response_model=List[GenrePartial])
+@cache(expire=REDIS_CACHE_EXPIRE)
 async def genres(
     page_size: int = Query(50, alias='page[size]'),
     page_number: int = Query(1, alias='page[number]'),
@@ -33,6 +37,7 @@ async def genres(
     return [ GenrePartial(**genre.dict()) for genre in genres ]
 
 @router.get('/{genre_id}/films', response_model=List[PartialFilmInfo])
+@cache(expire=REDIS_CACHE_EXPIRE)
 async def genre_films(
     genre_id: str,
     page_size: int = Query(50, alias='page[size]'),
