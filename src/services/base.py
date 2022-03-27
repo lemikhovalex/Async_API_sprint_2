a996f29c0e0cb694a583ea2a973010983fe09d77
Gee Hjwić <ghj096@gmail.com>
2022-03-27 19:01:54 +0300

    Remove caching from service

--- src/services/base.py
+++ src/services/base.py
@@ -1,7 +1,6 @@
 from typing import List, Optional
 
 from abc import ABC, abstractmethod
-from aioredis import Redis
 from elasticsearch import AsyncElasticsearch, exceptions
 from models.base import BaseModel
 
@@ -9,8 +8,7 @@ CACHE_EXPIRE_IN_SECONDS = 1
 
 
 class BaseService(ABC):
-    def __init__(self, redis: Redis, elastic: AsyncElasticsearch):
-        self.redis = redis
+    def __init__(self, elastic: AsyncElasticsearch):
         self.elastic = elastic
 
     async def get_by(self, page_number: int, page_size: int, sort: str = None, **kwargs) -> List[BaseModel]:
@@ -36,19 +34,6 @@ class BaseService(ABC):
             for doc in result['hits']['hits'] ]
 
     async def get_by_id(self, entity_id: str) -> Optional[BaseModel]:
-        entity = await self._get_from_cache(entity_id)
-        if entity:
-            return entity
-        # no entity in cache
-        entity = await self._get_from_elastic(entity_id)
-        if not entity:
-            return None
-
-        await self._put_to_cache(entity)
-
-        return entity
-
-    async def _get_from_elastic(self, entity_id: str) -> Optional[BaseModel]:
         try:
             doc = await self.elastic.get(
                 index=self._index_name(),
@@ -57,12 +42,6 @@ class BaseService(ABC):
         except exceptions.NotFoundError:
             return None
         return self._result_class().parse_obj(doc['_source'])
-
-    async def _get_from_cache(self, entity_id: str) -> Optional[BaseModel]:
-        return None
-        
-    async def _put_to_cache(self, entity: BaseModel):
-        return None
 
     @abstractmethod
     def _index_name(self):
--- src/services/persons.py
+++ src/services/persons.py
@@ -1,8 +1,6 @@
 from functools import lru_cache
 
-from aioredis import Redis
 from db.elastic import get_elastic
-from db.redis import get_redis
 from elasticsearch import AsyncElasticsearch
 from fastapi import Depends
 from models.person import Person
@@ -24,7 +22,6 @@ class PersonService(BaseService):
 
 @lru_cache()
 def get_person_service(
-    redis: Redis = Depends(get_redis),
     elastic: AsyncElasticsearch = Depends(get_elastic),
 ) -> PersonService:
-    return PersonService(redis, elastic)
+    return PersonService(elastic)
