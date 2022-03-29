dc71a756951dd3fb35284be9dc0583f906c92da9
Aleksand Lemikhov <lemikhovalex@gmail.com>
2022-03-29 19:32:55 +0300

    fix issue_37

--- src/api/v1/films.py
+++ src/api/v1/films.py
@@ -12,21 +12,26 @@ from services.films import FilmService, get_film_service
 router = APIRouter()
 
 
-@router.get("/{film_id}", response_model=FilmFullInfo)
+@router.get("/search", response_model=List[PartialFilmInfo])
 @cache(expire=REDIS_CACHE_EXPIRE)
-async def film_details(
-    film_id: str, film_service: FilmService = Depends(get_film_service)
-) -> FilmFullInfo:
-    film = await film_service.get_by_id(film_id)
-    if not film:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="film not found")
-    return FilmFullInfo.parse_obj(film.dict())
+async def film_search(
+    query: Optional[str] = None,
+    page: dict = Depends(get_page_params),
+    film_service: FilmService = Depends(get_film_service),
+    filter_genre: Optional[UUID] = Query(None, alias="filter[genre]"),
+) -> List[PartialFilmInfo]:
+    return await get_all_search(
+        film_service=film_service,
+        query=query,
+        page=page,
+        filter_genre=filter_genre,
+    )
 
 
 @router.get("", response_model=List[PartialFilmInfo])
 @cache(expire=REDIS_CACHE_EXPIRE)
 async def film_search_general(
-    sort: Optional[str] = None,
+    sort: Optional[str] = "imdb_rating",
     page: dict = Depends(get_page_params),
     film_service: FilmService = Depends(get_film_service),
     filter_genre: Optional[UUID] = Query(None, alias="filter[genre]"),
@@ -39,21 +44,15 @@ async def film_search_general(
     )
 
 
-@router.get("/search", response_model=List[PartialFilmInfo])
+@router.get("/{film_id}", response_model=FilmFullInfo)
 @cache(expire=REDIS_CACHE_EXPIRE)
-async def film_search(
-    query: Optional[str] = None,
-    page: dict = Depends(get_page_params),
-    film_service: FilmService = Depends(get_film_service),
-    filter_genre: Optional[UUID] = Query(None, alias="filter[genre]"),
-) -> List[PartialFilmInfo]:
-
-    return await get_all_search(
-        film_service=film_service,
-        query=query,
-        page=page,
-        filter_genre=filter_genre,
-    )
+async def film_details(
+    film_id: UUID, film_service: FilmService = Depends(get_film_service)
+) -> FilmFullInfo:
+    film = await film_service.get_by_id(film_id)
+    if not film:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="film not found")
+    return FilmFullInfo.parse_obj(film.dict())
 
 
 async def get_all_search(
