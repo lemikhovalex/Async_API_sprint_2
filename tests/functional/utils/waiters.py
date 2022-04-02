cfc319dc9d81ed4b665e396404857e3d31068526
Aleksandr Lemikhov <lemikhovalex@gmail.com>
2022-04-02 20:03:53 +0300

    close waiters

--- tests/functional/utils/waiters.py
+++ tests/functional/utils/waiters.py
@@ -14,9 +14,11 @@ async def wait_for_es():
     es = AsyncElasticsearch(url)
     while not (await es.ping()):
         await asyncio.sleep(10)
+    await es.close()
 
 
 async def wait_for_redis():
     redis = Redis(host=SETTINGS.redis_host, port=SETTINGS.redis_port)
     while not (await redis.ping()):
         await asyncio.sleep(10)
+    await redis.close()
