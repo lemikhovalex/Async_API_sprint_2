9cc24bed59eafe7774566dbea140bb5c7c398687
Aleksandr Lemikhov <lemikhovalex@gmail.com>
2022-04-02 20:03:42 +0300

    wait async

--- tests/functional/waiting.py
+++ tests/functional/waiting.py
@@ -2,7 +2,10 @@ import asyncio
 
 from utils.waiters import wait_for_es, wait_for_redis
 
+
+async def main():
+    await asyncio.gather(wait_for_es(), wait_for_redis())
+
+
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(asyncio.gather(wait_for_es(), wait_for_redis()))
-    loop.close()
+    asyncio.run(main())
