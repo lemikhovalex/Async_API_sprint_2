fce469b35d23cb571c843834e8fb9a0a0b8c968a
Aleksandr Lemikhov <lemikhovalex@gmail.com>
2022-04-04 22:05:53 +0300

    - wait for idx to refresh - create/delete idx async - fix py 3.10 warnings - add config for tests- correct reqs for pre commit hooks

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -16,7 +16,9 @@ SETTINGS = TestSettings()
 
 @pytest.fixture(scope="session")
 def event_loop():
-    return asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
 
 
 @pytest_asyncio.fixture(scope="session")
@@ -36,25 +38,35 @@ async def filled_es(
     es_connection: AsyncElasticsearch,
 ) -> AsyncGenerator[AsyncElasticsearch, None]:
     indecies = ["genres", "persons", "movies"]
+    await asyncio.gather(
+        *[
+            es_connection.indices.create(
+                index=idx,
+                settings=constants.settings,
+                mappings=getattr(constants, f"mappings_{idx}"),
+                ignore=HTTPStatus.BAD_REQUEST,
+            )
+            for idx in indecies
+        ]
+    )
     actions = []
     for idx in indecies:
-        await es_connection.indices.create(
-            index=idx,
-            settings=constants.settings,
-            mappings=getattr(constants, f"mappings_{idx}"),
-            ignore=HTTPStatus.BAD_REQUEST,
-        )
         actions.extend(actions_for_es_bulk(idx))
-    await asyncio.sleep(5)
+
     await async_bulk(client=es_connection, actions=actions)
+
+    await asyncio.gather(
+        *[es_connection.indices.refresh(index=idx) for idx in indecies]
+    )
 
     yield es_connection
 
     for idx in indecies:
-        es_connection.indices.delete(
+        await es_connection.indices.delete(
             index=idx,
             ignore=[HTTPStatus.BAD_REQUEST, HTTPStatus.NOT_FOUND],
         )
+    await es_connection.close()
 
 
 def actions_for_es_bulk(index: str) -> List[dict]:
