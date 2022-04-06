1675b95072277a34c6f5317e6a9f459592784946
Gee Hjwić <ghj096@gmail.com>
2022-04-07 02:13:43 +0300

    Refactor tests infrastructure to load es data in every test

--- tests/functional/src/api/v1/genres_test.py
+++ tests/functional/src/api/v1/genres_test.py
@@ -1,8 +1,11 @@
 import pytest
+from test_data import genres
+from utils import es_load
 
 
 @pytest.mark.asyncio
-async def test_genre_by_id(filled_es, make_get_request):
+async def test_genre_by_id(es_client, make_get_request):
+    await es_load(es_client, "genres", genres.genres)
     response = await make_get_request("/genres/1f64e918-b298-11ec-90b3-00155db24537")
     assert response.status == 200
     assert response.body == {
