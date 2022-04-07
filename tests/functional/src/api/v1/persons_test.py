0b7353daa593dc936c10fe6b1995e1861142191a
Gee Hjwić <ghj096@gmail.com>
2022-04-08 01:24:22 +0300

    Add persons tests

---
+++ tests/functional/src/api/v1/persons_test.py
@@ -0,0 +1,94 @@
+import pytest
+from test_data import movies, persons
+from utils import es_load, filter_uuid
+
+
+@pytest.mark.asyncio
+async def test_person_by_id(es_client, make_get_request):
+    await es_load(es_client, "persons", persons.persons)
+    response = await make_get_request("/persons/1f64ea08-b298-11ec-90b3-00155db24537")
+    assert response.status == 200
+    assert response.body == {
+        "uuid": "1f64ea08-b298-11ec-90b3-00155db24537",
+        "full_name": "Multitask person 0",
+    }
+    response = await make_get_request("/persons/0004ea08-b298-11ec-90b3-00155db24537")
+    assert response.status == 404
+
+
+@pytest.mark.asyncio
+async def test_persons(es_client, make_get_request):
+    await es_load(es_client, "persons", persons.persons)
+    response = await make_get_request("/persons")
+    assert response.status == 200
+    assert isinstance(response.body, list)
+    assert len(response.body) == 21
+
+
+@pytest.mark.asyncio
+async def test_persons_empty(es_client, make_get_request):
+    response = await make_get_request("/persons")
+    assert response.status == 200
+    assert response.body == []
+
+
+@pytest.mark.asyncio
+async def test_persons_pagination(es_client, make_get_request):
+    await es_load(es_client, "persons", persons.persons)
+    response = await make_get_request("/persons", {"page[size]": 20, "page[number]": 1})
+    assert response.status == 200
+    assert isinstance(response.body, list)
+    assert len(response.body) == 20
+    response = await make_get_request("/persons", {"page[size]": 20, "page[number]": 2})
+    assert response.status == 200
+    assert isinstance(response.body, list)
+    assert len(response.body) == 1
+
+
+@pytest.mark.asyncio
+async def test_persons_films(es_client, make_get_request):
+    await es_load(es_client, "persons", persons.persons)
+    await es_load(es_client, "movies", movies.movies)
+    response = await make_get_request(
+        "/persons/1f64ea08-b298-11ec-90b3-00155db24537/films"
+    )
+    assert response.status == 200
+    assert isinstance(response.body, list)
+    assert len(response.body) == 6
+    assert filter_uuid(response.body) == set(
+        [
+            "1f650754-b298-11ec-90b3-00155db24537",
+            "1f651c76-b298-11ec-90b3-00155db24537",
+            "1f652f72-b298-11ec-90b3-00155db24537",
+            "1f6546ba-b298-11ec-90b3-00155db24537",
+            "1f656672-b298-11ec-90b3-00155db24537",
+            "1f657e5a-b298-11ec-90b3-00155db24537",
+        ]
+    )
+
+
+@pytest.mark.asyncio
+async def test_genre_films_pagination(es_client, make_get_request):
+    await es_load(es_client, "persons", persons.persons)
+    await es_load(es_client, "movies", movies.movies)
+    response = await make_get_request(
+        "/persons/1f64ea08-b298-11ec-90b3-00155db24537/films",
+        {"page[size]": 3, "page[number]": 1},
+    )
+    assert response.status == 200
+    assert isinstance(response.body, list)
+    assert len(response.body) == 3
+    response = await make_get_request(
+        "/persons/1f64ea08-b298-11ec-90b3-00155db24537/films",
+        {"page[size]": 3, "page[number]": 2},
+    )
+    assert response.status == 200
+    assert isinstance(response.body, list)
+    assert len(response.body) == 3
+    response = await make_get_request(
+        "/persons/1f64ea08-b298-11ec-90b3-00155db24537/films",
+        {"page[size]": 3, "page[number]": 3},
+    )
+    assert response.status == 200
+    assert isinstance(response.body, list)
+    assert len(response.body) == 0
