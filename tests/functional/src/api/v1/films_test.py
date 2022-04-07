e489af0d10ab6523e07a5d154636b859f4a8de2d
Aleksandr Lemikhov <lemikhovalex@gmail.com>
2022-04-07 21:42:29 +0300

    check all wilms and pagination

--- tests/functional/src/api/v1/films_test.py
+++ tests/functional/src/api/v1/films_test.py
@@ -1,14 +1,28 @@
+import copy
 import json
+from operator import attrgetter
 
 import pytest
+from pydantic import BaseModel
 from utils import es_load
+
+
+class PartialFilm(BaseModel):
+    imdb_rating: float
+    title: str
+    uuid: str
+
+    class Config:
+        fields = {"uuid": "id"}
+
+
+with open("test_data/movies.json", "r") as f:
+    MOVIES = json.load(f)
 
 
 @pytest.mark.asyncio
 async def test_film_by_id(es_client, make_get_request):
-    with open("test_data/movies.json", "r") as f:
-        movies = json.load(f)
-    await es_load(es_client, "movies", movies)
+    await es_load(es_client, "movies", MOVIES)
     response = await make_get_request("/films/1f6546ba-b298-11ec-90b3-00155db24537")
     assert response.status == 200
     assert response.body == {
@@ -78,3 +92,48 @@ async def test_film_by_id(es_client, make_get_request):
             },
         ],
     }
+
+
+@pytest.mark.asyncio
+async def test_films_pagination(es_client, make_get_request):
+    await es_load(es_client, "movies", MOVIES)
+    resp_all_films = await make_get_request(
+        "/films/?sort=imdb_rating&page[size]=6&page[number]=1"
+    )
+    assert resp_all_films.status == 200
+    all_ids = {film["uuid"] for film in resp_all_films.body}
+
+    resps_films_by_parts = []
+    n_parts = 2
+    for _i in range(n_parts):
+        resp = await make_get_request(
+            f"/films/?sort=imdb_rating&page[size]=3&page[number]={_i+1}"
+        )
+        assert resp.status == 200
+        resps_films_by_parts.append([film["uuid"] for film in resp.body])
+
+    for part in range(n_parts):
+        for r in resps_films_by_parts[part]:
+            assert r in all_ids
+
+    for part in range(n_parts):
+        for second_p in range(part, n_parts):
+            bool(set(resps_films_by_parts[part]) & set(resps_films_by_parts[second_p]))
+
+
+@pytest.mark.asyncio
+async def test_films_check_all_films(es_client, make_get_request):
+
+    await es_load(es_client, "movies", MOVIES)
+    resp_all_films = await make_get_request(
+        "/films/?sort=imdb_rating&page[size]=1000&page[number]=1"
+    )
+    assert resp_all_films.status == 200
+    assert isinstance(resp_all_films.body, list)
+    assert len(resp_all_films.body) == 6
+    _MOVIES = copy.deepcopy(MOVIES)
+    _MOVIES = [PartialFilm(**m) for m in _MOVIES]
+    _MOVIES = sorted(_MOVIES, key=attrgetter("uuid"))
+    _MOVIES = sorted(_MOVIES, key=attrgetter("imdb_rating"), reverse=True)
+    _MOVIES = [json.loads(f.json()) for f in _MOVIES]
+    assert resp_all_films.body == _MOVIES
