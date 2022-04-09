79f7e81ff39d69d0f1cc85bef9d32d8fa9cf3cca
Aleksandr Lemikhov <lemikhovalex@gmail.com>
2022-04-09 17:25:03 +0300

    refactor paginator

--- src/services/films.py
+++ src/services/films.py
@@ -1,17 +1,14 @@
 from functools import lru_cache
-from math import ceil
 from typing import List, Optional, Type
 from uuid import UUID
 
 from elasticsearch import AsyncElasticsearch
-from elasticsearch.exceptions import NotFoundError
 from fastapi import Depends
 
-from core.config import MAX_ES_SEARCH_FROM_SIZE
 from db.elastic import get_elastic
 from models.film import Film
-
-from .base import BaseService
+from services.base import BaseService
+from services.paginators import ESQueryPaginator
 
 
 class FilmService(BaseService):
@@ -38,15 +35,6 @@ class FilmService(BaseService):
 
         return {"bool": {"should": [_q_nested(i, person_id) for i in roles]}}
 
-    async def get_by_id(self, film_id: UUID) -> Optional[Film]:
-        out = None
-        try:
-            doc = await self.elastic.get(index=self._index_name(), id=str(film_id))
-            out = Film(**doc["_source"])
-        except NotFoundError:
-            pass
-        return out
-
     async def get_by_query(
         self,
         query: Optional[str] = None,
@@ -59,18 +47,17 @@ class FilmService(BaseService):
         if unique_sort_fields is None:
             unique_sort_fields = ["id"]
 
-        body = {
-            "sort": [
-                {
-                    "_score": {},
-                },
-            ],
-            "query": {
-                "bool": {"must": [], "should": []},
+        _sort = [
+            {
+                "_score": {},
             },
+        ]
+
+        _query = {
+            "bool": {"must": [], "should": []},
         }
         if query is not None:
-            body["query"]["bool"]["must"].append(
+            _query["bool"]["must"].append(
                 {
                     "multi_match": {
                         "query": query,
@@ -78,9 +65,8 @@ class FilmService(BaseService):
                     },
                 }
             )
-
         if genre_filter is not None:
-            body["query"]["bool"]["must"].append(
+            _query["bool"]["must"].append(
                 {
                     "nested": {
                         "path": "genres",
@@ -94,12 +80,13 @@ class FilmService(BaseService):
             if sort_by.startswith("-"):
                 order = "asc"
                 sort_by = sort_by[1:]
-            body["sort"].insert(0, {sort_by: {"order": order}})
+            _sort.insert(0, {sort_by: {"order": order}})
 
-        body["sort"].extend([{_sf: {}} for _sf in unique_sort_fields])
+        _sort.extend([{_sf: {}} for _sf in unique_sort_fields])
 
-        paginator = QueryPaginator(
-            body=body,
+        paginator = ESQueryPaginator(
+            query=_query,
+            sort=_sort,
             es=self.elastic,
             index=self._index_name(),
             page_number=page_number,
@@ -115,56 +102,3 @@ def get_film_service(
     elastic: AsyncElasticsearch = Depends(get_elastic),
 ) -> FilmService:
     return FilmService(elastic)
-
-
-class QueryPaginator:
-    def __init__(
-        self,
-        body: dict,
-        es: AsyncElasticsearch,
-        index: str,
-        page_number: int,
-        page_size: int,
-    ):
-        self.es = es
-        self.body = body
-        self.index = index
-        self.page_size = page_size
-        self.search_from = (page_number - 1) * page_size
-        self.accum_shift = 0
-        self.search_after = None
-        self.pit = None
-
-    async def paginate_query(self) -> dict:
-        n = ceil(self.search_from / MAX_ES_SEARCH_FROM_SIZE)
-        self.pit = await self.es.open_point_in_time(index=self.index, keep_alive="1m")
-        self.pit = self.pit["id"]
-        # make shure that search after point to the beginning of page
-        for _ in range(n):
-            await self._process_inner_pag_query()
-
-        self.body["size"] = self.page_size
-
-        out = await self.es.search(
-            body=self.body, pit={"id": self.pit, "keep_alive": "1m"}
-        )
-        await self.es.close_point_in_time(id=self.pit)
-        self.pit = None
-        return out
-
-    async def _process_inner_pag_query(self) -> None:
-        if self.search_after is None:
-            self.body["size"] = min(self.search_from, MAX_ES_SEARCH_FROM_SIZE)
-            self.body["from"] = 0
-        else:
-            self.body["size"] = min(
-                MAX_ES_SEARCH_FROM_SIZE, self.search_from - self.accum_shift
-            )
-        resp = await self.es.search(
-            body=self.body, pit={"id": self.pit, "keep_alive": "1m"}
-        )
-        self.accum_shift += len(resp["hits"]["hits"])
-        self.search_after = resp["hits"]["hits"][-1]["sort"]
-        self.body["search_after"] = self.search_after
-        if "from" in self.body:
-            del self.body["from"]
---
+++ src/services/paginators.py
@@ -0,0 +1,72 @@
+from math import ceil
+from typing import Optional
+
+from elasticsearch import AsyncElasticsearch
+
+from core.config import MAX_ES_SEARCH_FROM_SIZE
+
+
+class ESQueryPaginator:
+    def __init__(
+        self,
+        query: dict,
+        sort: list,
+        es: AsyncElasticsearch,
+        index: str,
+        page_number: int,
+        page_size: int,
+        **kwargs
+    ):
+        self.query = query
+        self.sort = sort
+        self.es = es
+        self.index = index
+        self.page_size = page_size
+        self.search_from = (page_number - 1) * page_size
+        self.accum_shift = 0
+        self.search_after = None
+        self.pit = None
+        self.search_add_args = kwargs
+
+    async def paginate_query(self) -> dict:
+        n = ceil(self.search_from / MAX_ES_SEARCH_FROM_SIZE)
+        self.pit = await self.es.open_point_in_time(index=self.index, keep_alive="1m")
+        self.pit = self.pit["id"]
+        # make shure that search after point to the beginning of page
+        for _ in range(n):
+            await self._process_inner_pag_query()
+
+        out = await self._search_after()
+        await self.es.close_point_in_time(id=self.pit)
+        self.pit = None
+        return out
+
+    async def _process_inner_pag_query(self) -> None:
+        if self.search_after is None:
+            resp = await self._initial_query()
+        else:
+            size = min(MAX_ES_SEARCH_FROM_SIZE, self.search_from - self.accum_shift)
+            resp = await self._search_after(size=size)
+
+        self.accum_shift += len(resp["hits"]["hits"])
+        self.search_after = resp["hits"]["hits"][-1]["sort"]
+
+    async def _search_after(self, size: Optional[int] = None):
+        return await self.es.search(
+            query=self.query,
+            sort=self.sort,
+            search_after=self.search_after,
+            size=size or self.page_size,
+            pit={"id": self.pit, "keep_alive": "1m"},
+            **self.search_add_args
+        )
+
+    async def _initial_query(self):
+        return await self.es.search(
+            query=self.query,
+            sort=self.sort,
+            from_=0,
+            size=min(self.search_from, MAX_ES_SEARCH_FROM_SIZE),
+            pit={"id": self.pit, "keep_alive": "1m"},
+            **self.search_add_args
+        )
