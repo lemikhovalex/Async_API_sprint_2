14d37c11b93eefda1ca90d46d8c6a73434341028
Aleksandr Lemikhov <lemikhovalex@gmail.com>
2022-04-10 10:25:05 +0300

    keep one base class

--- src/services/base.py
+++ src/services/base.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import List, Optional, Type
+from typing import Optional, Type
 from uuid import UUID
 
 from elasticsearch import AsyncElasticsearch, exceptions
@@ -10,25 +10,7 @@ from services.paginators import BasePaginator
 CACHE_EXPIRE_IN_SECONDS = 1
 
 
-class BaseTableService(ABC):
-    @abstractmethod
-    async def get_by(self):
-        pass
-
-    @abstractmethod
-    async def get_by_id(self):
-        pass
-
-    @abstractmethod
-    def source() -> str:
-        pass
-
-    @abstractmethod
-    def _result_class() -> Type[BaseModel]:
-        pass
-
-
-class BaseESService(BaseTableService, ABC):
+class BaseService(ABC):
     elastic: AsyncElasticsearch
     paginator: Type[BasePaginator]
 
@@ -61,55 +43,13 @@ class BaseESService(BaseTableService, ABC):
             query=_query,
             sort=_sort,
             es=self.elastic,
-            index=self.source(),
+            index=self._index_name(),
             page_number=page_number,
             page_size=page_size,
         )
         resp = await paginator.paginate_query()
         results_src = [datum["_source"] for datum in resp["hits"]["hits"]]
         return [self._result_class().parse_obj(src) for src in results_src]
-
-    async def get_by_id(self, entity_id: UUID) -> Optional[BaseModel]:
-        try:
-            doc = await self.elastic.get(
-                index=self.source(),
-                id=str(entity_id),
-            )
-        except exceptions.NotFoundError:
-            return None
-        return self._result_class().parse_obj(doc["_source"])
-
-
-class BaseService(ABC):
-    def __init__(self, elastic: AsyncElasticsearch):
-        self.elastic = elastic
-
-    async def get_by(
-        self, page_number: int, page_size: int, sort: Optional[str] = None, **kwargs
-    ) -> List[BaseModel]:
-        """Random query fields goes in kwargs"""
-        if len(kwargs):
-            method_name = "_query_by_{0}".format("_".join(sorted(kwargs.keys())))
-            query = getattr(self, method_name)(**kwargs)
-        else:
-            query = {"match_all": {}}
-        sort_for_es = None
-        if sort is not None:
-            sort_for_es = {
-                sort.lstrip("-"): {"order": "desc" if sort.startswith("-") else "asc"}
-            }
-
-        result = await self.elastic.search(
-            index=self._index_name(),
-            query=query,
-            from_=page_size * (page_number - 1),
-            size=page_size,
-            sort=sort_for_es,
-        )
-        return [
-            self._result_class().parse_obj(doc["_source"])
-            for doc in result["hits"]["hits"]
-        ]
 
     async def get_by_id(self, entity_id: UUID) -> Optional[BaseModel]:
         try:
