66a6e7fdb7482eb17880d0504e22c767c8d75f73
Gee Hjwić <ghj096@gmail.com>
2022-04-11 04:01:41 +0300

    Reduce redis and es status check interval

--- tests/functional/utils/waiters.py
+++ tests/functional/utils/waiters.py
@@ -13,12 +13,12 @@ async def wait_for_es():
     url = f"http://{SETTINGS.es_host}:{SETTINGS.es_port}"
     es = AsyncElasticsearch(url)
     while not (await es.ping()):
-        await asyncio.sleep(10)
+        await asyncio.sleep(1)
     await es.close()
 
 
 async def wait_for_redis():
     redis = Redis(host=SETTINGS.redis_host, port=SETTINGS.redis_port)
     while not (await redis.ping()):
-        await asyncio.sleep(10)
+        await asyncio.sleep(1)
     await redis.close()
