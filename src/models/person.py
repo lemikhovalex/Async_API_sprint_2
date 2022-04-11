83d14a9b46ac36d20ff2f260f0ed0fb1ab39a15e
Gee Hjwić <ghj096@gmail.com>
2022-04-11 03:09:54 +0300

    Simplify model class names

--- src/models/person.py
+++ src/models/person.py
@@ -3,12 +3,12 @@ from typing import List
 from .base import BaseModel
 
 
-class IDNamePerson(BaseModel):
+class BasePerson(BaseModel):
     full_name: str
 
     class Config:
         fields = {"uuid": "id", "full_name": "name"}
 
 
-class Person(IDNamePerson):
+class Person(BasePerson):
     roles: List[str]
