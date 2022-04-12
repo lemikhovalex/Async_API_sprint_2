6bc6d30ec6096ca7e5372d619295c28d99f2670b
Gee Hjwić <ghj096@gmail.com>
2022-04-12 22:44:28 +0300

    Use status code constants instead of integer values

--- tests/functional/src/api/v1/genres_test.py
+++ tests/functional/src/api/v1/genres_test.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 import pytest
 from test_data import genres, movies
 from utils import es_load, filter_uuid
@@ -7,14 +9,14 @@ from utils import es_load, filter_uuid
 async def test_genre_by_id_absent(es_client, make_get_request):
     await es_load(es_client, "genres", genres.genres)
     response = await make_get_request("genres/1f64e918-0000-11ec-90b3-00155db24537")
-    assert response.status == 404
+    assert response.status == HTTPStatus.NOT_FOUND
 
 
 @pytest.mark.asyncio
 async def test_genre_by_id(es_client, make_get_request):
     await es_load(es_client, "genres", genres.genres)
     response = await make_get_request("genres/1f64e918-b298-11ec-90b3-00155db24537")
-    assert response.status == 200
+    assert response.status == HTTPStatus.OK
     assert response.body == {
         "uuid": "1f64e918-b298-11ec-90b3-00155db24537",
         "name": "action",
@@ -25,7 +27,7 @@ async def test_genre_by_id(es_client, make_get_request):
 async def test_genres(es_client, make_get_request):
     await es_load(es_client, "genres", genres.genres)
     response = await make_get_request("genres")
-    assert response.status == 200
+    assert response.status == HTTPStatus.OK
     assert isinstance(response.body, list)
     assert len(response.body) == 3
 
@@ -33,7 +35,7 @@ async def test_genres(es_client, make_get_request):
 @pytest.mark.asyncio
 async def test_genres_empty(es_client, make_get_request):
     response = await make_get_request("genres")
-    assert response.status == 200
+    assert response.status == HTTPStatus.OK
     assert response.body == []
 
 
@@ -41,11 +43,11 @@ async def test_genres_empty(es_client, make_get_request):
 async def test_genres_pagination(es_client, make_get_request):
     await es_load(es_client, "genres", genres.genres)
     response = await make_get_request("genres", {"page[size]": 2, "page[number]": 1})
-    assert response.status == 200
+    assert response.status == HTTPStatus.OK
     assert isinstance(response.body, list)
     assert len(response.body) == 2
     response = await make_get_request("genres", {"page[size]": 2, "page[number]": 2})
-    assert response.status == 200
+    assert response.status == HTTPStatus.OK
     assert isinstance(response.body, list)
     assert len(response.body) == 1
 
@@ -57,7 +59,7 @@ async def test_genre_films(es_client, make_get_request):
     response = await make_get_request(
         "genres/1f64e918-b298-11ec-90b3-00155db24537/films"
     )
-    assert response.status == 200
+    assert response.status == HTTPStatus.OK
     assert isinstance(response.body, list)
     assert len(response.body) == 3
     assert filter_uuid(response.body) == set(
@@ -77,13 +79,13 @@ async def test_genre_films_pagination(es_client, make_get_request):
         "genres/1f64e918-b298-11ec-90b3-00155db24537/films",
         {"page[size]": 2, "page[number]": 1},
     )
-    assert response.status == 200
+    assert response.status == HTTPStatus.OK
     assert isinstance(response.body, list)
     assert len(response.body) == 2
     response = await make_get_request(
         "genres/1f64e918-b298-11ec-90b3-00155db24537/films",
         {"page[size]": 2, "page[number]": 2},
     )
-    assert response.status == 200
+    assert response.status == HTTPStatus.OK
     assert isinstance(response.body, list)
     assert len(response.body) == 1
--- tests/functional/src/api/v1/persons_test.py
+++ tests/functional/src/api/v1/persons_test.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 import pytest
 from test_data import movies, persons
 from utils import es_load, filter_uuid
@@ -7,14 +9,14 @@ from utils import es_load, filter_uuid
 async def test_person_by_id_absent(es_client, make_get_request):
     await es_load(es_client, "persons", persons.persons)
     response = await make_get_request("persons/1f64ea08-0000-11ec-90b3-00155db24537")
-    assert response.status == 404
+    assert response.status == HTTPStatus.NOT_FOUND
 
 
 @pytest.mark.asyncio
 async def test_person_by_id(es_client, make_get_request):
     await es_load(es_client, "persons", persons.persons)
     response = await make_get_request("persons/1f64ea08-b298-11ec-90b3-00155db24537")
-    assert response.status == 200
+    assert response.status == HTTPStatus.OK
     assert response.body == {
         "uuid": "1f64ea08-b298-11ec-90b3-00155db24537",
         "full_name": "Multitask person 0",
@@ -25,7 +27,7 @@ async def test_person_by_id(es_client, make_get_request):
 async def test_persons(es_client, make_get_request):
     await es_load(es_client, "persons", persons.persons)
     response = await make_get_request("persons")
-    assert response.status == 200
+    assert response.status == HTTPStatus.OK
     assert isinstance(response.body, list)
     assert len(response.body) == 21
 
@@ -33,7 +35,7 @@ async def test_persons(es_client, make_get_request):
 @pytest.mark.asyncio
 async def test_persons_empty(es_client, make_get_request):
     response = await make_get_request("persons")
-    assert response.status == 200
+    assert response.status == HTTPStatus.OK
     assert response.body == []
 
 
@@ -41,11 +43,11 @@ async def test_persons_empty(es_client, make_get_request):
 async def test_persons_pagination(es_client, make_get_request):
     await es_load(es_client, "persons", persons.persons)
     response = await make_get_request("persons", {"page[size]": 20, "page[number]": 1})
-    assert response.status == 200
+    assert response.status == HTTPStatus.OK
     assert isinstance(response.body, list)
     assert len(response.body) == 20
     response = await make_get_request("persons", {"page[size]": 20, "page[number]": 2})
-    assert response.status == 200
+    assert response.status == HTTPStatus.OK
     assert isinstance(response.body, list)
     assert len(response.body) == 1
 
@@ -57,7 +59,7 @@ async def test_persons_films(es_client, make_get_request):
     response = await make_get_request(
         "persons/1f64ea08-b298-11ec-90b3-00155db24537/films"
     )
-    assert response.status == 200
+    assert response.status == HTTPStatus.OK
     assert isinstance(response.body, list)
     assert len(response.body) == 6
     assert filter_uuid(response.body) == set(
@@ -80,20 +82,20 @@ async def test_genre_films_pagination(es_client, make_get_request):
         "persons/1f64ea08-b298-11ec-90b3-00155db24537/films",
         {"page[size]": 3, "page[number]": 1},
     )
-    assert response.status == 200
+    assert response.status == HTTPStatus.OK
     assert isinstance(response.body, list)
     assert len(response.body) == 3
     response = await make_get_request(
         "persons/1f64ea08-b298-11ec-90b3-00155db24537/films",
         {"page[size]": 3, "page[number]": 2},
     )
-    assert response.status == 200
+    assert response.status == HTTPStatus.OK
     assert isinstance(response.body, list)
     assert len(response.body) == 3
     response = await make_get_request(
         "persons/1f64ea08-b298-11ec-90b3-00155db24537/films",
         {"page[size]": 3, "page[number]": 3},
     )
-    assert response.status == 200
+    assert response.status == HTTPStatus.OK
     assert isinstance(response.body, list)
     assert len(response.body) == 0
