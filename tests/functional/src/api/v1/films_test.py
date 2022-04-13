f8ad9071f1ff0993ea0acae9c5f1a2b22d1419d9
Aleksandr Lemikhov <lemikhovalex@gmail.com>
2022-04-13 19:56:59 +0300

    add parametrization for pagination

--- tests/functional/src/api/v1/films_test.py
+++ tests/functional/src/api/v1/films_test.py
@@ -22,6 +22,52 @@ class PartialFilm(BaseModel):
 pytestmark = pytest.mark.asyncio
 
 
+TEST_FILMS_PAFINATION_DATA = [
+    (
+        1,
+        3,
+        [
+            {
+                "uuid": "1f650754-b298-11ec-90b3-00155db24537",
+                "title": "HP 1",
+                "imdb_rating": 10.0,
+            },
+            {
+                "uuid": "1f656672-b298-11ec-90b3-00155db24537",
+                "title": "SW 1",
+                "imdb_rating": 10.0,
+            },
+            {
+                "uuid": "1f651c76-b298-11ec-90b3-00155db24537",
+                "title": "HP 2",
+                "imdb_rating": 9.9,
+            },
+        ],
+    ),
+    (
+        2,
+        3,
+        [
+            {
+                "uuid": "1f657e5a-b298-11ec-90b3-00155db24537",
+                "title": "SW 2",
+                "imdb_rating": 9.9,
+            },
+            {
+                "uuid": "1f652f72-b298-11ec-90b3-00155db24537",
+                "title": "HP 3",
+                "imdb_rating": 9.8,
+            },
+            {
+                "uuid": "1f6546ba-b298-11ec-90b3-00155db24537",
+                "title": "HP 4",
+                "imdb_rating": 9.7,
+            },
+        ],
+    ),
+]
+
+
 async def test_film_by_id_absent(es_client, make_get_request):
     await es_load(es_client, "movies", movies)
 
@@ -105,31 +151,28 @@ async def test_film_by_id(es_client, make_get_request):
     }
 
 
-async def test_films_pagination(es_client, make_get_request):
+def pagination_test_id_gen(x):
+    if isinstance(x, int):
+        return x
+
+
+@pytest.mark.parametrize(
+    "page_num,page_size,resp", TEST_FILMS_PAFINATION_DATA, ids=pagination_test_id_gen
+)
+async def test_films_pagination(page_num, page_size, resp, es_client, make_get_request):
     await es_load(es_client, "movies", movies)
-    resp_all_films = await make_get_request(
-        "films", params={"sort": "imdb_rating", "page[size]": 6, "page[number]": 1}
+
+    films_resp = await make_get_request(
+        "films",
+        params={
+            "sort": "imdb_rating",
+            "page[size]": page_size,
+            "page[number]": page_num,
+        },
     )
-    assert resp_all_films.status == HTTPStatus.OK
-    all_ids = {film["uuid"] for film in resp_all_films.body}
 
-    first_page_resp = await make_get_request(
-        "films/?sort=imdb_rating&page[size]=3&page[number]=1"
-    )
-    assert first_page_resp.status == HTTPStatus.OK
-
-    first_page = {film["uuid"] for film in first_page_resp.body}
-    assert first_page.issubset(all_ids)
-
-    second_page_resp = await make_get_request(
-        "films/?sort=imdb_rating&page[size]=3&page[number]=2"
-    )
-    assert second_page_resp.status == HTTPStatus.OK
-
-    second_page = {film["uuid"] for film in second_page_resp.body}
-    assert second_page.issubset(all_ids)
-
-    assert len(second_page.intersection(first_page)) == 0
+    assert films_resp.status == HTTPStatus.OK
+    assert films_resp.body == resp
 
 
 async def test_films_check_all_films(es_client, make_get_request):
