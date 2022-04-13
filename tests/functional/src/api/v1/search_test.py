f44b2007431d93f4faa9f33bb2fd320fd381a089
Aleksandr Lemikhov <lemikhovalex@gmail.com>
2022-04-13 20:23:23 +0300

    AAA for search

--- tests/functional/src/api/v1/search_test.py
+++ tests/functional/src/api/v1/search_test.py
@@ -7,6 +7,36 @@ from utils import es_load
 # All test coroutines will be treated as marked with this decorator.
 pytestmark = pytest.mark.asyncio
 
+TEST_SEARCH_FILMS_PAFINATION_DATA = [
+    (
+        1,
+        2,
+        [
+            {
+                "uuid": "1f650754-b298-11ec-90b3-00155db24537",
+                "title": "HP 1",
+                "imdb_rating": 10.0,
+            },
+            {
+                "uuid": "1f652f72-b298-11ec-90b3-00155db24537",
+                "title": "HP 3",
+                "imdb_rating": 9.8,
+            },
+        ],
+    ),
+    (
+        2,
+        2,
+        [
+            {
+                "uuid": "1f6546ba-b298-11ec-90b3-00155db24537",
+                "title": "HP 4",
+                "imdb_rating": 9.7,
+            }
+        ],
+    ),
+]
+
 
 async def test_persons_multitask(es_client, make_get_request):
     await es_load(es_client, "persons", persons.persons)
@@ -123,27 +153,33 @@ async def test_films_with_genre_2(es_client, make_get_request):
     ]
 
 
-async def test_films_with_genre_2_with_pagination(es_client, make_get_request):
+def search_pagination_test_id_gen(x):
+    if isinstance(x, int):
+        return x
+
+
+@pytest.mark.parametrize(
+    "page_num,page_size,resp",
+    TEST_SEARCH_FILMS_PAFINATION_DATA,
+    ids=search_pagination_test_id_gen,
+)
+async def test_films_with_genre_2_with_pagination(
+    page_num, page_size, resp, es_client, make_get_request
+):
     await es_load(es_client, "movies", movies.movies)
 
     response = await make_get_request(
         "films/search",
         params={
             "query": "HP",
-            "page[number]": 2,
-            "page[size]": 2,
+            "page[number]": page_num,
+            "page[size]": page_size,
             "filter[genre]": "1f64e56c-b298-11ec-90b3-00155db24537",
         },
     )
 
     assert response.status == HTTPStatus.OK
-    assert response.body == [
-        {
-            "uuid": "1f6546ba-b298-11ec-90b3-00155db24537",
-            "title": "HP 4",
-            "imdb_rating": 9.7,
-        },
-    ]
+    assert response.body == resp
 
 
 async def test_films_not_existing(es_client, make_get_request):
